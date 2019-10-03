066072aca8c62928a6159a6fdd33c625eb7b1147
OttoCodeBerlin <ottoschmidberlin@gmail.com>
2019-10-03 16:49:21 -0500

    Changed Firebase data location

--- frontend/src/firebase-config.js
+++ frontend/src/firebase-config.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app'
 import 'firebase/storage'
 
   const config = {
-    apiKey: "AIzaSyD4HPMPBtPWnyo1L5EKMaok6WiN04whQ7s",
-    authDomain: "ocb-store-management.firebaseapp.com",
-    databaseURL: "https://ocb-store-management.firebaseio.com",
-    projectId: "ocb-store-management",
-    storageBucket: "ocb-store-management.appspot.com",
-    messagingSenderId: "195114045052",
-    appId: "1:195114045052:web:73572d83ce44e93e23c3f9",
-    measurementId: "G-CNYE2NM3MC"
+    apiKey: process.env.FIREBASE_apiKey,
+    authDomain: process.env.FIREBASE_authDomain,
+    databaseURL: process.env.FIREBASE_databaseURL,
+    projectId: process.env.FIREBASE_projectId,
+    storageBucket: process.env.FIREBASE_storageBucket,
+    messagingSenderId: process.env.FIREBASE_messagingSenderId,
+    appId: process.env.FIREBASE_appId,
+    measurementId: process.env.FIREBASE_measurementId
   };
   
   firebase.initializeApp(config);
